8684bc127ac1c4610de5f1148d0b091300e7f44a
Austin <austin@c4saas.com>
2026-03-16 04:33:21 +0000

    fix: remove stale AdminQuickLinkCard re-export that broke docker build

--- client/src/components/admin/index.ts
+++ client/src/components/admin/index.ts
@@ -2,6 +2,5 @@ export * from './AdminHeader';
 export * from './AdminHeaderMenu';
 export * from './AdminSectionTabs';
 export * from './AdminCard';
-export * from './AdminQuickLinkCard';
 export * from './AdminSettingsErrorState';
 export * from './AdminSidebar';
